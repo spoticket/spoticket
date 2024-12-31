34070970328e35480caf12918dcb3a2fedc86564
cylcoder <cylcoder@gmail.com>
2025-01-01 00:23:29 +0900

    feat: GameResponse DTO 클래스 생성

---
+++ com.spoticket.games/src/main/java/com/spoticket/game/dto/response/GameResponse.java
@@ -0,0 +1,37 @@
+package com.spoticket.game.dto.response;
+
+import com.spoticket.game.domain.model.Game;
+import com.spoticket.game.domain.model.Sport;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Builder
+public class GameResponse {
+
+    private UUID gameId;
+    private String title;
+    private LocalDateTime startTime;
+    private Sport sport;
+    private String league;
+    private UUID stadiumId;
+    private UUID homeTeamId;
+    private UUID awayTeamId;
+
+    public static GameResponse from(Game game) {
+        return GameResponse.builder()
+                .gameId(game.getGameId())
+                .title(game.getTitle())
+                .startTime(game.getStartTime())
+                .sport(game.getSport())
+                .league(game.getLeague())
+                .stadiumId(game.getStadiumId())
+                .homeTeamId(game.getHomeTeamId())
+                .awayTeamId(game.getAwayTeamId())
+                .build();
+    }
+
+}
--- com.spoticket.games/src/main/java/com/spoticket/game/test/TestController.java
+++ com.spoticket.games/src/main/java/com/spoticket/game/test/TestController.java
@@ -2,6 +2,7 @@ package com.spoticket.game.test;
 
 import com.spoticket.game.domain.model.Game;
 import com.spoticket.game.domain.repository.GameRepository;
+import com.spoticket.game.dto.response.GameResponse;
 import com.spoticket.game.global.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,10 +22,10 @@ public class TestController {
     private final GameRepository gameRepository;
 
     @GetMapping("/api/v1/games/test")
-    public DataResponse<Game> testGame() {
+    public DataResponse<GameResponse> testGame() {
         String uuid = "83f697f8-0d32-4cd6-97a1-5a8c970e1ee7";
         Game game = gameRepository.findById(UUID.fromString(uuid)).orElseThrow();
-        return ok(game);
+        return ok(GameResponse.from(game));
     }
 
     @GetMapping("/api/v1/games/exception")
