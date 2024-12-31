858f90a4ee8e873489c54f732e0a3eac7f230079
cylcoder <cylcoder@gmail.com>
2024-12-31 14:16:06 +0900

    Setup: MSA 서비스 필수 의존성 및 설정 구성

---
+++ com.spoticket.games/src/main/java/com/spoticket/game/TestController.java
@@ -0,0 +1,16 @@
+package com.spoticket.game;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class TestController {
+
+    @GetMapping("/api/v1/games/test")
+    public String testGame() {
+        return "TestController.testGame";
+    }
+
+}
---
+++ com.spoticket.team-stadium/src/main/java/com/spoticket/teamstadium/TeamStadiumApplication.java
@@ -0,0 +1,13 @@
+package com.spoticket.teamstadium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamStadiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamStadiumApplication.class, args);
+	}
+
+}
