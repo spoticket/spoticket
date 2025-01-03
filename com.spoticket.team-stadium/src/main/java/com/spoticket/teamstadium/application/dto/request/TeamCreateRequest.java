d8f453efe0d2907598141e1a44badd7f2c78bf8a
le-monaaa <kgbn013@naver.com>
2025-01-03 19:28:41 +0900

    fix: TEXT 타입 입력 값 검증 삭제

--- com.spoticket.team-stadium/src/main/java/com/spoticket/teamstadium/application/dto/request/TeamCreateRequest.java
+++ com.spoticket.team-stadium/src/main/java/com/spoticket/teamstadium/application/dto/request/TeamCreateRequest.java
@@ -9,7 +9,7 @@ public record TeamCreateRequest(
 
     @NotBlank @Size(max = 100) String name,
     @NotNull TeamCategoryEnum category,
-    @NotBlank @Size(max = 500) String description,
+    String description,
     @Size(max = 500) String profile,
     @Size(max = 300) String homeLink,
     @Size(max = 300) String snsLink
--- com.spoticket.team-stadium/src/main/java/com/spoticket/teamstadium/application/dto/request/TeamUpdateRequest.java
+++ com.spoticket.team-stadium/src/main/java/com/spoticket/teamstadium/application/dto/request/TeamUpdateRequest.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.Size;
 
 public record TeamUpdateRequest(
     @Size(max = 100) String name,
-    @Size(max = 500) String description,
+    String description,
     @Size(max = 500) String profile,
     @Size(max = 300) String homeLink,
     @Size(max = 300) String snsLink
