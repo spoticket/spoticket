795d72b940fb75aaf9884eae4c775ec2caec59be
cylcoder <cylcoder@gmail.com>
2025-01-04 02:35:46 +0900

    feat: 경기 알림 구현

---
+++ com.spoticket.games/src/main/java/com/spoticket/game/dto/response/TicketResponse.java
@@ -0,0 +1,17 @@
+package com.spoticket.game.dto.response;
+
+import com.spoticket.game.temp.TicketStatus;
+import java.util.UUID;
+
+public record TicketResponse(
+
+    UUID ticketId,
+    UUID userId,
+    UUID gameId,
+    UUID seatId,
+    String seatName,
+    TicketStatus status
+    
+) {
+
+}
