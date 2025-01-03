627626d8913ae3c51a77daab63b2cae71a1d5616
seven2762 <seven2762@gmail.com>
2025-01-03 16:35:47 +0900

    [feat][#45]빌더 어노테이션 추가

--- com.spoticket.payment/src/main/java/com/spoticket/payment/domain/order/model/OrderItem.java
+++ com.spoticket.payment/src/main/java/com/spoticket/payment/domain/order/model/OrderItem.java
@@ -4,14 +4,21 @@ import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "P_ORDER_ITEMS", schema = "payment_service")
 public class OrderItem {
     @Id @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "order_item_id")
     private UUID orderItemId;
-
-    private UUID orderId;
 
     @Column(nullable = false)
     private UUID ticketId;
