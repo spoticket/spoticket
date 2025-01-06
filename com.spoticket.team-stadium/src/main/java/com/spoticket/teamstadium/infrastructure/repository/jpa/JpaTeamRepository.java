16e3e6d01e6e746987184aa9ed2347ba5ba704a6
le-monaaa <kgbn013@naver.com>
2025-01-07 02:56:34 +0900

    feat: 팀 검색 기능
    - 팀 검색 기능 구현

--- com.spoticket.team-stadium/src/main/java/com/spoticket/teamstadium/infrastructure/repository/jpa/JpaTeamRepository.java
+++ com.spoticket.team-stadium/src/main/java/com/spoticket/teamstadium/infrastructure/repository/jpa/JpaTeamRepository.java
@@ -3,6 +3,7 @@ package com.spoticket.teamstadium.infrastructure.repository.jpa;
 
 import com.spoticket.teamstadium.domain.model.Team;
 import com.spoticket.teamstadium.domain.repository.TeamRepository;
+import com.spoticket.teamstadium.infrastructure.repository.queryDsl.TeamRepositoryCustom;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -14,7 +15,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface JpaTeamRepository extends TeamRepository, JpaRepository<Team, UUID> {
+public interface JpaTeamRepository extends TeamRepository, JpaRepository<Team, UUID>,
+    TeamRepositoryCustom {
 
   @Query("SELECT t FROM p_teams t WHERE REPLACE(t.name, ' ', '') = REPLACE(:name, ' ', '') AND t.isDeleted = false")
   Optional<Team> findByNameAndIsDeletedFalse(@Param("name") String name);
