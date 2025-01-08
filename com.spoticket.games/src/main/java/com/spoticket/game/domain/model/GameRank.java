f5027e93af6c1c3bc838f4da77a4d7e95850400e
cylcoder <cylcoder@gmail.com>
2025-01-08 12:44:17 +0900

    feat: 인기 경기 순위 엔티티 생성

---
+++ com.spoticket.games/src/main/java/com/spoticket/game/domain/model/GameRank.java
@@ -0,0 +1,35 @@
+package com.spoticket.game.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name = "p_game_ranks")
+@Builder
+public class GameRank {
+
+  @Id
+  @GeneratedValue(generator = "UUID")
+  private UUID gameRankId;
+
+  @OneToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "game_id")
+  private Game game;
+
+  private double score;
+
+}
