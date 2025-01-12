8aa98defa417306e5c27fcbcecd8bf4ba3cd8311
seven2762 <seven2762@gmail.com>
2025-01-12 21:36:43 +0900

    [feat][#30] 결제 상태값 메서드 추가

--- com.spoticket.payment/src/main/java/com/spoticket/payment/domain/payment/model/PaymentStatus.java
+++ com.spoticket.payment/src/main/java/com/spoticket/payment/domain/payment/model/PaymentStatus.java
@@ -5,12 +5,22 @@ import lombok.Getter;
 @Getter
 public enum PaymentStatus {
     PENDING("결제 대기"),
-    CANCEL("결제 취소"),
-    COMPLETED("결제 완료");
+    COMPLETED("결제 완료"),
+    CANCELLED("결제 취소"),
+    DONE("결제 승인 완료");
 
     private final String description;
 
     PaymentStatus(String description) {
         this.description = description;
     }
+
+
+public static PaymentStatus fromTossStatus(String tossStatus) {
+    return switch (tossStatus) {
+        case "DONE" -> COMPLETED;
+        case "CANCELED" -> CANCELLED;
+        default -> throw new IllegalArgumentException("알 수 없는 상태 값 : " + tossStatus);
+    };
+}
 }
