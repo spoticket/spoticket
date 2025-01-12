704e190975aa3af53622ba0254b90211746d25a4
seven2762 <seven2762@gmail.com>
2025-01-12 21:38:51 +0900

    [feat][#30] 결제 수단 상수 추가

--- com.spoticket.payment/src/main/java/com/spoticket/payment/domain/payment/model/PaymentMethod.java
+++ com.spoticket.payment/src/main/java/com/spoticket/payment/domain/payment/model/PaymentMethod.java
@@ -2,7 +2,8 @@ package com.spoticket.payment.domain.payment.model;
 
 public enum PaymentMethod {
     CARD("카드"),
-    TRANSFER("계좌이체");
+    TRANSFER("계좌이체"),
+    EASY_PAY("간편결제");
 
     PaymentMethod(String description) {
     }
