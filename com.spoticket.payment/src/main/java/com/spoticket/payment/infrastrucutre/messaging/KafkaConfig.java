82bb26154efa9fe99b217682718fee0077459b0e
seven2762 <seven2762@gmail.com>
2025-01-14 21:47:08 +0900

    [feat][#92] 카프카 설정 클래스

---
+++ com.spoticket.payment/src/main/java/com/spoticket/payment/infrastrucutre/messaging/KafkaConfig.java
@@ -0,0 +1,32 @@
+package com.spoticket.payment.infrastrucutre.messaging;
+
+
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import com.spoticket.payment.application.order.dto.OrderRes;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public ProducerFactory<String, OrderRes> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, OrderRes> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
