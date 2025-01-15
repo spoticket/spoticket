e5c08f9d36185504e6d421eeedc812d164f27646
yoaruku <rhrudgh12@naver.com>
2025-01-16 06:03:23 +0900

    fix #105: FIFO coupon issue by redis

--- com.spoticket.user/src/main/java/com/spoticket/user/global/config/SecurityConfig.java
+++ com.spoticket.user/src/main/java/com/spoticket/user/global/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.spoticket.user.global.util;
+package com.spoticket.user.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
