a8deec99d83ba51eaa98d6a9d82f4c5ac8e13dc6
seven2762 <seven2762@gmail.com>
2025-01-19 18:07:05 +0900

    [feat][#92] 뷰 컨트롤러 수정

--- com.spoticket.payment/src/main/java/com/spoticket/payment/presentation/payment/controller/PaymentViewController.java
+++ com.spoticket.payment/src/main/java/com/spoticket/payment/presentation/payment/controller/PaymentViewController.java
@@ -1,21 +1,30 @@
 package com.spoticket.payment.presentation.payment.controller;
 
-import com.spoticket.payment.application.order.service.OrderService;
+
+import com.spoticket.payment.application.payment.dto.PaymentEventRes;
+import com.spoticket.payment.application.payment.service.PaymentService;
+import com.spoticket.payment.domain.payment.model.PaymentConsumer;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 @Controller
 @RequiredArgsConstructor
 public class PaymentViewController {
 
+    private final PaymentService paymentService;
+
 
     @GetMapping("/payment")
-    public String paymentPage() {
+    public String paymentPage(@RequestParam UUID orderId, Model model) {
+        PaymentEventRes eventRes = paymentService.getEvent(orderId);
+        model.addAttribute("orderId",orderId );
+        model.addAttribute("amount",eventRes.getAmount());
+        model.addAttribute("itemName",eventRes.getItemName() );
         return "payment";
     }
 
