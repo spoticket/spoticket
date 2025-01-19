2fd0e9e6794b50b95f76c54fc3524679255ebada
seven2762 <seven2762@gmail.com>
2025-01-19 18:12:05 +0900

    [feat][#92] res dto 자료형 변경

--- com.spoticket.payment/src/main/java/com/spoticket/payment/infrastrucutre/toss/dto/TossPaymentRes.java
+++ com.spoticket.payment/src/main/java/com/spoticket/payment/infrastrucutre/toss/dto/TossPaymentRes.java
@@ -1,11 +1,13 @@
 package com.spoticket.payment.infrastrucutre.toss.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.time.OffsetDateTime;
+import java.util.UUID;
 import lombok.Builder;
 
 @Builder
-public record TossPaymentRes(String paymentKey, String orderId, String orderName, String status,
-                             Long totalAmount, OffsetDateTime approvedAt, String method, String description, Card card) {
+public record TossPaymentRes(String paymentKey, UUID orderId, String orderName, String status,
+                             Long totalAmount, OffsetDateTime approvedAt, String method, String description, @JsonIgnore Card card) {
 
     public record Card(String issuerCode, int installmentPlanMonths) {
 
