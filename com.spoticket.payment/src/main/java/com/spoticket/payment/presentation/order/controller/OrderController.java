7237aa1fdc5e1b29c6bea493bf6e952719e6ecf8
seven2762 <seven2762@gmail.com>
2025-01-19 18:11:32 +0900

    [feat][#92] 주문 컨트롤러 수정

--- com.spoticket.payment/src/main/java/com/spoticket/payment/presentation/order/controller/OrderController.java
+++ com.spoticket.payment/src/main/java/com/spoticket/payment/presentation/order/controller/OrderController.java
@@ -7,22 +7,27 @@ import com.spoticket.payment.application.order.service.OrderService;
 import com.spoticket.payment.presentation.common.ApiSuccessResponse;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/orders")
 public class OrderController {
 
     private final OrderService orderService;
-
     @PostMapping
-    public ApiSuccessResponse<OrderRes> createOrder(CreateOrderReq req) {
-       OrderRes orderRes =  orderService.createOrder(req);
+    public ApiSuccessResponse<OrderRes> createOrder(   @RequestHeader(value = "X-User-Id", required = true) UUID userId,
+        @RequestBody CreateOrderReq req) {
+        OrderRes orderRes =  orderService.createOrder(userId, req);
+
         return ApiSuccessResponse.success(orderRes);
     }
     @GetMapping("/{orderId}")
