05e85197c76c9dd24fb37a333deb5d0860430fb3
seven2762 <seven2762@gmail.com>
2025-01-19 18:13:08 +0900

    [feat][#92] 주문 생성 dto 변경

--- com.spoticket.payment/src/main/java/com/spoticket/payment/application/order/dto/CreateOrderReq.java
+++ com.spoticket.payment/src/main/java/com/spoticket/payment/application/order/dto/CreateOrderReq.java
@@ -1,18 +1,30 @@
 package com.spoticket.payment.application.order.dto;
 
-import com.spoticket.payment.domain.order.model.OrderItem;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class CreateOrderReq {
 
-    private UUID userID;
+    @JsonProperty("userId")
+    private UUID userId;
+    @JsonIgnore
     private UUID userCouponId;
-    private List<OrderItemReq> items;
+    private List<OrderItemReq> items = new ArrayList<>();
 
     @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class OrderItemReq {
         private UUID ticketId;
         private int price;
