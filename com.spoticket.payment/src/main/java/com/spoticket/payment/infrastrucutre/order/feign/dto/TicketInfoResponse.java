951b5518a0788108a7c31cd9d1a2b7ca47914aac
seven2762 <seven2762@gmail.com>
2025-01-20 12:40:41 +0900

    [feat][#92] 티켓 서비스 feign dto 클래스

---
+++ com.spoticket.payment/src/main/java/com/spoticket/payment/infrastrucutre/order/feign/dto/TicketInfoResponse.java
@@ -0,0 +1,19 @@
+package com.spoticket.payment.infrastrucutre.order.feign.dto;
+
+import com.spoticket.payment.infrastrucutre.order.feign.ticket.TicketStatus;
+import java.util.UUID;
+
+public record TicketInfoResponse (
+    UUID ticketId,
+    UUID userId,
+    UUID seatId,
+    String userName,
+    String gameName,
+    String stadiumName,
+    String section,
+    String seatName,
+    Integer price,
+    TicketStatus status
+
+){
+}
