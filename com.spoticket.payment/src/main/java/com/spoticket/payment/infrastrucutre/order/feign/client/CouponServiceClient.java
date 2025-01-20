2e55a47cca3c82e52ccab558fe66714b0d2126b0
seven2762 <seven2762@gmail.com>
2025-01-20 12:43:02 +0900

    [feat][#92] 유저 feign client prefix 수정

--- com.spoticket.payment/src/main/java/com/spoticket/payment/infrastrucutre/order/feign/client/CouponServiceClient.java
+++ com.spoticket.payment/src/main/java/com/spoticket/payment/infrastrucutre/order/feign/client/CouponServiceClient.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "coupon-service")
+@FeignClient(name = "user")
 public interface CouponServiceClient {
     
     @GetMapping("/api/v1/users/coupons/{userCouponId}")
