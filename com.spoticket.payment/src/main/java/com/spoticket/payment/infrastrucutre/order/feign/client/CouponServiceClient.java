bafcdf9f575bb3a5fa97b0df92c466b690d6a92e
seven2762 <seven2762@gmail.com>
2025-01-21 00:48:55 +0900

    [feat][#92] 쿠폰 feign client 반환 타입 수정

--- com.spoticket.payment/src/main/java/com/spoticket/payment/infrastrucutre/order/feign/client/CouponServiceClient.java
+++ com.spoticket.payment/src/main/java/com/spoticket/payment/infrastrucutre/order/feign/client/CouponServiceClient.java
@@ -2,6 +2,7 @@ package com.spoticket.payment.infrastrucutre.order.feign.client;
 
 
 import com.spoticket.payment.infrastrucutre.order.feign.dto.UserCouponResponseDto;
+import com.spoticket.payment.presentation.common.ApiSuccessResponse;
 import java.util.UUID;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface CouponServiceClient {
     
     @GetMapping("/api/v1/users/coupons/{userCouponId}")
-    UserCouponResponseDto getCoupon(@PathVariable UUID userCouponId);
+    ApiSuccessResponse<UserCouponResponseDto> getCoupon(@PathVariable UUID userCouponId);
     
 }
