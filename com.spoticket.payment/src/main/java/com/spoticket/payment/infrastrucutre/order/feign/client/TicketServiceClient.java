f208464724f2cc90cc768e0f61d4de46c1ff7e5e
seven2762 <seven2762@gmail.com>
2025-01-21 00:53:07 +0900

    [feat][#92] ticket 쪽 feign 엔드포인트 수정

--- com.spoticket.payment/src/main/java/com/spoticket/payment/infrastrucutre/order/feign/client/TicketServiceClient.java
+++ com.spoticket.payment/src/main/java/com/spoticket/payment/infrastrucutre/order/feign/client/TicketServiceClient.java
@@ -1,6 +1,7 @@
 package com.spoticket.payment.infrastrucutre.order.feign.client;
 
 import com.spoticket.payment.infrastrucutre.order.feign.dto.TicketInfoResponse;
+import com.spoticket.payment.presentation.common.ApiSuccessResponse;
 import java.util.UUID;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient("ticket")
 public interface TicketServiceClient {
 
-    @GetMapping("api/v1/ticket/{ticketId}")
-    TicketInfoResponse getTicket(@PathVariable("ticketId") UUID ticketId);
+    @GetMapping("/api/v1/tickets/{ticketId}")
+    ApiSuccessResponse<TicketInfoResponse> getTicket(@PathVariable("ticketId") UUID ticketId);
 }
